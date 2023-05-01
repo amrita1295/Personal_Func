646fc533165540ae32d4ec2dfa7c7cbe9c52384f
amrita jassal <amrita.singh@trainingmug.com>
2023-05-01 17:04:58 +0530

    media

--- src/Components/Personal/Personal/Media/Media.js
+++ src/Components/Personal/Personal/Media/Media.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import Slider from "react-slick";
 import './Media.css'
+import media from "../../../../assets/Media-1.png"
 const Media = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
     return (
-        <div>
-            <h1>Media</h1>
+        <div >
+        <Slider {...settings}>
+          <div >
+            <img src={media} alt='media-1'/>
+          </div>
+          <div>
+            <img src={media} alt='media-1'/>
+          </div>
+        </Slider>
+        <br />
         </div>
+      
     );
 };
 
