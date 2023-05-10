618c8eba2ad722c672e33dce30d3d1491141ea17
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2023-05-10 14:27:22 +0600

    mission-video-updated

--- src/Components/Personal/Personal/Vision/Vision.js
+++ src/Components/Personal/Personal/Vision/Vision.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Vision.css";
 import videoIcon from "../../../../assets/Video Icon.png";
 import video from "../../../../assets/rishi-video.mp4";
+import video1 from "../../../../assets/Let's make healthy happen.mp4";
 import { DefaultPlayer as Video } from "react-html5video";
 import "react-html5video/dist/styles.css";
 import thumb from "../../../../assets/thumb.png";
@@ -11,10 +12,10 @@ const Vision = () => {
     <div className="vision-bg">
       <h1 className="text-center vision-title">Our Mission</h1>
 
-      {/* <Video className="mx-auto mt-20 video" poster = {thumb}>
-        <source className="rounded-3xl" src={video} type="video/mp4" />
-      </Video> */}
-
+      <Video className="mx-auto mt-20 video" poster = {thumb}>
+        <source className="rounded-3xl" src={video1} type="video/mp4" />
+      </Video>
+{/* 
       <div className="main mt-10">
         <div className="flex justify-center  container-1">
           <Link
@@ -28,7 +29,7 @@ const Vision = () => {
             </div>
           </Link>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
