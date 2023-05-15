274f4579d6f46af976501cd74595adb2d748fb18
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2023-05-15 22:56:07 +0600

    gentle-parenting-picture added

--- src/Components/Personal/Personal/Blog/BlogComponents/GentleParenting.js
+++ src/Components/Personal/Personal/Blog/BlogComponents/GentleParenting.js
@@ -5,9 +5,8 @@ import arrow from "../../../../../assets/Arrow.png";
 const GentleParenting = () => {
   return (
     <div className="bg-[#FBF8F0] pb-12">
+       <img src={gentleParentingPic} alt="" />
       <div className="mx-auto w-3/4">
-        <img src={gentleParentingPic} alt="" />
-
         <div
           tabIndex={0}
           className="collapse collapse-open bg-[#FBF8F0] mt-10 "
